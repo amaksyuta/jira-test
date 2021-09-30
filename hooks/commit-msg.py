8208c2eddbb382a3ff35bccba1dfb904ef19bf0c
andrey.maksyuta <andrey.maksyuta@zultys.com>
2021-09-30 12:33:26 +0000

    JR: sasassasas.
    Signed-off-by: andrey.maksyuta <andrey.maksyuta@zultys.com>

--- hooks/commit-msg.py
+++ hooks/commit-msg.py
@@ -9,31 +9,46 @@ Check commit message according to guidelines
 import git
 import sys, re, subprocess
 
-repo = git.Repo(search_parent_directories=True)
-branch = repo.active_branch
+def main(message):
+    """Main function."""
+    #Required parts
+    repo = git.Repo(search_parent_directories=True)
+    branch = repo.active_branch
 
-print("[INFO]: Currnet branch name:" + branch.name)
-print("[INFO]: Start to commit the change in git")
+    print("[INFO]: Currnet branch name:" , branch.name)
+    projectIDs = ['WBS', 'MX']
+    jiraIDs = ['JR', 'jr']
 
-#Required parts
-requiredRegex = "^(JR|jr)\: [A-Z]-[0-9]+; ."
-requiredLength = 15
+    regexpJiraIDs = '|'.join(jiraIDs)
+    regexpProjectIDs = '|'.join(projectIDs)
 
-#Get the commit file
-commitMessageFile = open(sys.argv[1]) #The first argument is the file
-commitMessage = commitMessageFile.read().strip()
+    requiredRegex = '^((?:{})): ((?:{})-[\d]{{1,5}}); .+\.?$'.format(regexpJiraIDs, regexpProjectIDs)
+    requiredLength = 15
 
-if branch.name == "main":
-    print("[INFO]: Ready to commit the change in master")
-    sys.exit(0)
+    guidelineUrl = 'https://confluence.zultys.com:8443'
 
-if len(commitMessage) < requiredLength:
-    print("[ERROR]: Commit message is less than the required 15 characters.")
-    sys.exit(1)
+    #Get the commit file
+    commitMessageFile = open(message) #The first argument is the file
+    commitMessage = commitMessageFile.read().strip()
 
-if re.search(requiredRegex, commitMessage) is None:
-    print("[ERROR]: A JIRA Keyword must be filled with a commit")
-    sys.exit(1)
+    if branch.name == "main":
+        print("[INFO]: Ready to commit the change in master")
+        sys.exit(0)
+
+    if len(commitMessage) < requiredLength:
+        print("[ERROR]: Commit message is less than the required 15 characters.")
+        sys.exit(1)
+
+    if not re.match(requiredRegex, commitMessage):
+        sys.stderr.write("\n[ERROR]: Your commit message subject line does not follow the guideline\n")
+        sys.stderr.write("\n - Refer commit guideline: {}\n\n".format(guidelineUrl))
+        sys.exit(1)
+    else:
+        print("[INFO]: Commit message is validated")
+        sys.exit(0)
+            
     
-print("[INFO]: Commit message is validated")
-sys.exit(0)
+
+if __name__ == "__main__":
+    main(sys.argv[1])
+
